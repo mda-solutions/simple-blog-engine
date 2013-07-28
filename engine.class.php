1f9e058080389b9e1e11b397891fa05c7121edc3
kerberos <moises@ubuntu.(none)>
2013-07-27 21:42:01 -0500

    add licence

--- engine.class.php
+++ engine.class.php
@@ -1,4 +1,19 @@
 <?php
+
+/**
+ *
+ * Licensed under the MIT License (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
   
 class Engine
 {
@@ -85,7 +100,6 @@ class Engine
    {     
 
       $folder = $this->settings["folder_entries"];
-      //$cont   = 0;
       $posts  = new stdClass();
 
       foreach ($this->htmls as $html) 
